edeb48274b4e9d8d06a21f3ed23c8cfd37c8cab2
rono <rono_back@163.com>
2015-05-18 23:16:17 +0800

    Analyze Average part6 + description

--- StockData_sina/AnalyzeData/analyzeDailyData.cpp
+++ StockData_sina/AnalyzeData/analyzeDailyData.cpp
@@ -31,7 +31,7 @@ vector<averageData> *analyzeDailyData::GetDailyDataFromFile(int year, int data_J
 	return &_vecTmpDailyData;
 }
 // [IN] avgDay: the first col is average days, the following cols are weights
-// [*]weight is from previous to recent
+// [*]weight is from recent to previous
 int analyzeDailyData::GetnDayAverage(int *avgDay, float **avgWeight, vector<averageData> *avgData, int avgNum) {
 	int count = 0;
 	// initial tmp structure
@@ -46,6 +46,33 @@ int analyzeDailyData::GetnDayAverage(int *avgDay, float **avgWeight, vector<aver
 			memset(tmpData[i], 0, sizeof(averageData)*avgDay[i]);
 		}
 	}
+	/********************************************************************************/
+	/* average algorithm description
+	/* data: 5,4,3,2,1,... weight:0.5,0.3,0.2 avgDay[i]:3 (the ith calculation)
+	  --------------------------------------------------------------------
+	   count   idx_tmp       tmpData[i][0]      tmpData[i][1]      tmpData[i][2]
+                                clear()
+	     0        0             5*0.5(0)           5*0.2(2)           5*0.3(1)
+	 --------------------------------------------------------------------------------
+                                                   clear()
+		 1        1          5*0.5+4*0.3(1)        4*0.5(0)        5*0.3+4*0.2(2)
+	 --------------------------------------------------------------------------------
+                                                                      clear()
+		 2        2       5*0.5+4*0.3+3*0.2(2)   4*0.5+3*0.3(1)       3*0.5(0)
+		                      push_back(0)
+	 --------------------------------------------------------------------------------
+                                clear()
+		 3        0             2*0.5(0)      4*0.5+3*0.3+2*0.2(2)  3*0.5+2*0.3(1)
+		                                          push_back(1)
+	 --------------------------------------------------------------------------------
+                                                    clear()
+		 4        1          2*0.5+1*0.3(1)         1*0.5(0)      3*0.5+2*0.3+1*0.2(2)
+                                                                      push_back(2)
+	 --------------------------------------------------------------------------------
+       count count%avgDay[i]    tmpData[i][j] += avgData[i][count]*((idx_tmp-j)%avgDay[i])
+    idx_tmp.clear()
+    if (idx_tmp >= avgDay[i]-1) push_back((idx_tmp+1)%avgDay[i])
+	/********************************************************************************/
 	// main analyze procedure
 	int idx_tmp = 0, idx_wt = 0;
 	// loop each raw data
@@ -59,12 +86,12 @@ int analyzeDailyData::GetnDayAverage(int *avgDay, float **avgWeight, vector<aver
 			for (int j = 0; j < avgDay[i]; j++) {
 				idx_wt = (idx_tmp - j)%avgDay[i];
 				// average data = weight1*data1 + weight2*data2 + ...
-				tmpData[i][idx_tmp] = tmpData[i][idx_tmp] + ((*it)*avgWeight[i][idx_wt]);
+				tmpData[i][j] = tmpData[i][j] + ((*it)*avgWeight[i][idx_wt]);
 				if (avgDay[i]-1 == idx_wt) {
 					// if count less than needed average days, ignore it because it's incomplete
 					if (count >= avgDay[i]-1)
 						// record average data
-						avgData[i].push_back(tmpData[i][idx_tmp]);
+						avgData[i].push_back(tmpData[i][(idx_tmp+1)%avgDay[i]]);
 				}
 			}
 		}
