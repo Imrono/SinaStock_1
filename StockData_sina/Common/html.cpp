f40063559760910650ba888c75cdf4ecab39b79a
rono@homePC <rono_back@163.com>
2015-05-31 12:11:29 +0800

    html part2

--- StockData_sina/Common/html.cpp
+++ StockData_sina/Common/html.cpp
@@ -1,38 +1,128 @@
 ﻿#include "html.h"
 
-const char *const MY_HTML::td_col_lt = "<td";
-const char *const MY_HTML::td_col_rt = "</td>";
-const char *const MY_HTML::th_col_lt = "<th";
-const char *const MY_HTML::th_col_rt = "</th>";
-const char *const MY_HTML::tr_row_lt = "<td";
-const char *const MY_HTML::tr_row_rt = "</td>";
-const char *const MY_HTML::thread_head_lt = "<thead";
-const char *const MY_HTML::thread_head_rt = "</thead>";
-const char *const MY_HTML::table_tb_lt = "<table";
-const char *const MY_HTML::table_tb_rt = "</thead>";
+const char *const MY_HTML::td_lt = "<td";
+const char *const MY_HTML::td_rt = "</td>";
+const char *const MY_HTML::th_lt = "<th";
+const char *const MY_HTML::th_rt = "</th>";
+const char *const MY_HTML::tr_start = "<tr";
+const char *const MY_HTML::tr_end = "</tr>";
+const int MY_HTML::ColRowLtLen = 3;
+const int MY_HTML::ColRowRtLen = 4;
+const char *const MY_HTML::thead_start = "<thead";
+const char *const MY_HTML::thead_end = "</thead>";
+const char *const MY_HTML::table_start = "<table";
+const char *const MY_HTML::table_end = "</table>";
+const char *const MY_HTML::tbody_lt = "<table";
+const char *const MY_HTML::tbody_rt = "</table>";
+const char *const MY_HTML::tfoot_lt = "<table";
+const char *const MY_HTML::tfoot_rt = "</table>";
+const int MY_HTML::SegmentLtLen = 6;
+const int MY_HTML::SegmentRtLen = 8;
 
-
-void MY_HTML::getTable(const char *const str, int length) {
+void MY_HTML::getTable(char *const str, int length) {
+	const int searchScope = 32;
 	int tdCount = 0;
 	int thCount = 0;
 	int trCount = 0;
-	int threadCount = 0;
 
-	const char *tmp;
+	char *tmp;
 	const char *tmp_end;
-	// check基本格式
-	if (nullptr == (tmp = strstr(str, MY_HTML::table_tb_lt))) {
+	char *table_end;
+	char tmpStoreTable;
+	char tmpStoreSegment;
+	const char *MarkStart;
+	char *MarkEnd;
+	const char *SegStart;
+	char *SegEnd;
+	vector<StrElem> *RowAns;
+
+	// check table的基本格式
+	if (nullptr == (tmp = strstr(str, MY_HTML::table_start))) {
 		ERRR("None table label start\n");
 		return;
 	} else {
-		if (nullptr == (tmp_end = strstr(str, MY_HTML::table_tb_rt))) {
+		if (nullptr == (table_end = strstr(str, MY_HTML::table_end))) {
 			ERRR("None table label end\n");
 			return;
 		} else {
-			tmp_end += strlen(MY_HTML::table_tb_rt);
+			table_end += MY_HTML::SegmentRtLen;
+			_setZeroAfter(table_end);
 		}
 	}
 	// 得到table head
-	if (1);
+	while (nullptr != (tmp = strstr(tmp, MY_HTML::thead_start))) {
+		SegStart = tmp;
+		if (nullptr == (SegEnd = strstr(tmp, MY_HTML::thead_end))) {
+			ERRR("None table label end\n");
+			return;
+		} else {
+			SegEnd += MY_HTML::SegmentLtLen;
+			_setZeroAfter(SegEnd);
 
+			MarkStart = strstr(tmp, MY_HTML::tr_start);
+			tmp = strstr(tmp, MY_HTML::tr_end);
+			tmp += MY_HTML::ColRowRtLen; // tmp在SegEnd
+			MarkEnd = tmp;
+			_setZeroAfter(MarkEnd);
+
+			// 求解detail table
+			RowAns = getRow(MarkStart, MarkEnd, th_type);
+			// 存储
+			tableEle[head_type].push_back(rowEle);
+			rowEle.clear();
+			// 恢复之前状态
+			_recoverDataAfter(); // mark
+			MarkStart = nullptr; MarkEnd = nullptr;
+			_recoverDataAfter(); // seg
+			SegStart = nullptr; SegEnd = nullptr;
+		}
+	}
+
+	// 得到table body
+
+
+	// 得到table foot
+
+
+	_recoverDataAfter(); // table
+}
+
+// start = "<tr", end = "<\tr>"的最后
+vector<StrElem> *MY_HTML::getRow(const char *start, const char *end, int td_th) {
+	if (nullptr == start || nullptr == end) return nullptr;
+
+	int colCount = 0;
+	StrElem tmpColStr;
+	memset(&tmpColStr, 0, sizeof(StrElem));
+
+	const char *colTypeStart = nullptr;
+	const char *colTypeEnd = nullptr;
+	if (td_type == td_th) {
+		colTypeStart = MY_HTML::td_lt;
+		colTypeEnd = MY_HTML::td_rt;
+	} else if (th_type == td_th) {
+		colTypeStart = MY_HTML::th_lt;
+		colTypeEnd = MY_HTML::td_rt;
+	} else INFO("None COL type, known colType is td or th\n");
+
+	for (const char *tmp = start; tmp < end;) {
+		if (nullptr == (tmp = strstr(tmp, colTypeStart))) goto ERROR_CASE;
+		tmp += MY_HTML::ColRowLtLen;
+		if (nullptr == (tmp = strchr(tmp, '>'))) goto ERROR_CASE;
+		while ('<' == *(tmp+1)) {
+			if (nullptr == (tmp = strchr(tmp, '>'))) goto ERROR_CASE;
+			else continue;
+		}
+		tmpColStr.Elem = tmp+1;
+		if (nullptr == (tmp = strchr(tmp, '<'))) goto ERROR_CASE;
+		tmpColStr.Length = tmp - tmpColStr.Elem;
+		rowEle.push_back(tmpColStr);
+		memset(&tmpColStr, 0, sizeof(StrElem));
+		if (nullptr == (tmp = strstr(tmp, colTypeEnd))) goto ERROR_CASE;
+		else continue;
+	}
+	return &rowEle;
+
+ERROR_CASE:
+	return nullptr;
 }
--- StockData_sina/Common/html.h
+++ StockData_sina/Common/html.h
@@ -1,27 +1,76 @@
 ﻿#include <string>
+#include <vector>
 using namespace std;
 #include "TraceMicro.h"
 
+struct StrElem {
+	const char *Elem;
+	int Length;
+};
+struct tmpStoreReplace {
+	char *Location;
+	char c;
+};
+
 class MY_HTML
 {
-	inline static bool getRow(const char *str, char *start, char *end) {
-
-	}
+	vector<StrElem> *getRow(const char *start, const char *end, int td_th);
 	inline static bool getCol(const char *str, char *start, char *end) {
 
 	}
 
-	void getTable(const char *const str, int length);
+	void getTable(char *const str, int length);
+
+	enum ColType {
+		th_type = 0,
+		td_type = 1
+	};
+	enum RowType {
+		head_type = 0,
+		body_type = 1,
+		foot_type = 2
+	};
 
 public:
-	static const char *const td_col_rt;
-	static const char *const td_col_lt;
-	static const char *const th_col_rt;
-	static const char *const th_col_lt;
-	static const char *const tr_row_rt;
-	static const char *const tr_row_lt;
-	static const char *const thread_head_rt;
-	static const char *const thread_head_lt;
-	static const char *const table_tb_rt;
-	static const char *const table_tb_lt;
+	// head body foot分开
+	vector<vector<StrElem>> tableEle[3];
+	vector<StrElem> rowEle;
+
+	static const char *const td_rt;
+	static const char *const td_lt;
+	static const char *const th_rt;
+	static const char *const th_lt;
+	static const char *const tr_end;
+	static const char *const tr_start;
+	static const char *const thead_head_end;
+	static const char *const thead_head_start;
+	static const char *const table_start;
+	static const char *const table_end;
+	static const int ColRowLtLen;
+	static const int ColRowRtLen;
+	static const char *const thead_start;
+	static const char *const thead_end;
+	static const char *const table_lt;
+	static const char *const table_rt;
+	static const char *const tbody_lt;
+	static const char *const tbody_rt;
+	static const char *const tfoot_lt;
+	static const char *const tfoot_rt;
+	static const int SegmentLtLen;
+	static const int SegmentRtLen;
+
+private:
+	vector<tmpStoreReplace> _tmpStore;
+	void _setZeroAfter(char *tmp) {
+		tmpStoreReplace x;
+		x.c = *(tmp+1);
+		x.Location = tmp+1;
+		_tmpStore.push_back(x);
+		*(tmp+1) = '\0';
+	}
+	void _recoverDataAfter() {
+		tmpStoreReplace x = _tmpStore.back();
+		_tmpStore.pop_back();
+		*x.Location = x.c;
+	}
 };
